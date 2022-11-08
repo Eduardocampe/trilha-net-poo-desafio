d37519fdfff82c4ec331ff2719779e883cb0a9aa
Eduardo Campestrini <eduardocampe123@hotmail.com>
2022-11-08 16:57:21 -0300

    Desafio Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone  Iphone1 = new Iphone("988246473");
+Iphone1.Ligar();
+Iphone1.ReceberLigacao();
+Iphone1.InstalarAplicativo("APP de banco.");
+
+Nokia nokia1 = new Nokia("988364321");
+nokia1.Ligar();
+nokia1.ReceberLigacao();
+nokia1.InstalarAplicativo("APP de Motos");
+
